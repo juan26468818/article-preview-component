71b17ff2f5c6784a9e04242921bcdfac0b09ea22
juan <juanayala@gmail.com>
2022-10-13 00:25:29 -0300

    fix bottom position on mobile view

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const shareContainer = document.getElementById("share-view")
 const shareI = document.getElementById("share-i")
 const shareB = document.getElementById("share-b")
+const shareT = document.getElementById("t-share")
+
 let isActive = false;
 
 shareB.addEventListener("click", ()=>{
         shareI.className += " active-i"
         shareB.className += " active-b"
+        shareContainer.classList.remove("hidden")
+        shareT.classList.remove("hidden")
         if(isActive === true){
             shareI.classList.remove("active-i")
             shareB.classList.remove("active-b")
+            shareContainer.className += " hidden"
+            shareT.className += " hidden"
             isActive = false
         }else{
             isActive = true
